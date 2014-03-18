5a5444bffd3b31f7c667baae3a9c9f41c6e3600e
Julien Garet <julien@garet.info>
2014-03-18 11:55:03 +0100

    exit with return code for nagios

--- check_xenserver.py
+++ check_xenserver.py
@@ -432,7 +432,9 @@ if __name__ == "__main__":
 			session=XenAPI.Session('https://'+e.details[1])
 			session.xenapi.login_with_password(username, password)
 		else:
-			raise
+			print "CRITICAL - XenAPI Error : " + e.details[0]
+			sys.exit(2)
+			
 	except:
 		print "CRITICAL - Connection Error"
 		sys.exit(2)
